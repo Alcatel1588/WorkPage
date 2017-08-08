f602c631efd1f4df6896a5abc28f86a784bad5cd
Lengesov <lengesovanton@gmail.com>
2017-08-08 18:54:13 +0300

    init commit

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function(){
+			// hide #back-top first
+			$("#back-top").hide();
+			$("#back-hadoop").hide();
+			
+			// fade in #back-top
+			$(function () {
+				$(window).scroll(function () {
+					if ($(this).scrollTop() > 1000) 
+						{
+						$('#back-top').fadeIn();
+						} else 
+						{
+							$('#back-top').fadeOut();
+						}
+					});
+				$(window).scroll(function () {
+					if ($(this).scrollTop() > 1000) 
+						{
+						$('#back-hadoop').fadeIn();
+						} else 
+						{
+							$('#back-hadoop').fadeOut();
+						}
+					});
+					// scroll body to 0px on click
+				$('#back-top a').click(function () {
+					$('body,html').animate({
+						scrollTop: 1000
+						}, 800);
+				});
+				$('#back-hadoop a').click(function () {
+					$('body,html').animate({
+						scrollTop: 2700
+						}, 800);
+				return false;
+				});
+				$('#hadoop_p span').click(function () {
+					$('body,html').animate({
+						scrollTop: 2700
+						}, 800);
+				return false;
+				});
+				
+			});
+		});
+				
