0db40dbeeaafc07e34cce857ba8b1b8d4e5b7272
Lengesov Anton <lengesovanton@gmail.com>
2017-08-16 19:48:28 +0300

    add tag in html document and popup menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 
 $(document).ready(function(){
 			// hide #back-top first
+			PopUpHide();
 			$("#back-top").hide();
 			$("#back-hadoop").hide();
 			new Clipboard('.btn-clipboard');
+			
+			var div = document.createElement('div');
+			div.className = "alert alert-success";
+			div.innerHTML = "Winter is coming ... ";
+
+			document.body.appendChild(div);
 			
 			// fade in #back-top
 			$(function () {
@@ -45,5 +52,14 @@ $(document).ready(function(){
 				});
 				
 			});
+			
 		});
 				
+			//Функция отображения PopUp
+			function PopUpShow(){
+				$("#popup1").show();
+			}
+			//Функция скрытия PopUp
+			function PopUpHide(){
+				$("#popup1").hide();
+			}
